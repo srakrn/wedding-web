25f648cb126179236ae7c3170eda5b7a36970b07
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2023-07-17 20:27:30 +0700

    Supabase client

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
+import { env } from '$env/dynamic/private';
 
 
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = env.SUPABASE_URL;
+const supabaseKey = env.SUPABASE_KEY
 export const supabase = createClient(supabaseUrl, supabaseKey)
-
