5141783da91755b591019502a822dd38696c246e
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2023-07-18 18:38:04 +0700

    Simple footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,9 @@
 </script>
 
 <slot />
+
+<footer class="footer mt-20 p-10 bg-neutral text-neutral-content">
+	<div>
+		Site made with love by Sirakorn L. Powered by SvelteKit + Supabase + Tailwing + daisyUI.
+	</div>
+</footer>
