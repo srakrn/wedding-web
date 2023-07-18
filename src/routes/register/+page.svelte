40246968a356d41f6272192210176cb32ae5a6a0
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2023-07-18 16:21:21 +0700

    Trying some forms

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -9,13 +9,18 @@
 <p>Lorem Ipsum Dolar</p>
 
 <form method="post">
-	<label
-		>You are
-		<input name="screen_name" value={data.screen_name} />
-	</label>
-	<label
-		>How many guests?
-		<input name="n_guests" />
-	</label>
-	<input type="submit" value="Register!" class="btn btn-primary" />
+	<div class="form-control">
+		<label class="label">
+			You are
+			<input name="screen_name" class="input input-bordered max-w-xs" value={data.screen_name} />
+		</label>
+		<label class="label">
+			How many guests?
+			<input name="n_guests" class="input input-bordered max-w-xs" value={data.screen_name} />
+		</label>
+		<label class="label">
+			<span class="label-text">What is your name?</span>
+			<input type="text" placeholder="Type here" class="input input-bordered max-w-xs" />
+		</label>
+	</div>
 </form>
