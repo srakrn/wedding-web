8e0583a6a291e345707f645588cca703caa6f823
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2023-08-06 20:50:50 +0700

    Simple error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div class="grid h-screen place-items-center">
+	<span class="text-center">
+		<div class="text-9xl">{$page.status} :(</div>
+		<div>Perhaps what you are looking for is not here.</div>
+	</span>
+</div>
