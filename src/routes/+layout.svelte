2c64e122e9973801c40be3c008eb2637edc3a9b7
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2023-08-06 20:50:54 +0700

    Remove footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,3 @@
 </script>
 
 <slot />
-
-<footer class="footer mt-20 p-5 bg-neutral text-neutral-content">
-	<div>
-		Site made with love by Sirakorn L. Powered by SvelteKit + Supabase + Tailwind + daisyUI.
-	</div>
-</footer>
