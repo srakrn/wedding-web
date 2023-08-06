4782c14b6e285140f2fc1719f3314fa3d9e80316
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2023-08-06 16:59:38 +0700

    Typo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,6 @@
 
 <footer class="footer mt-20 p-5 bg-neutral text-neutral-content">
 	<div>
-		Site made with love by Sirakorn L. Powered by SvelteKit + Supabase + Tailwing + daisyUI.
+		Site made with love by Sirakorn L. Powered by SvelteKit + Supabase + Tailwind + daisyUI.
 	</div>
 </footer>
