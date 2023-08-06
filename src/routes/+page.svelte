b9886a234ac854de650f30559bdbf112308040c6
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2023-08-06 20:50:59 +0700

    Main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="hero h-screen" style="background-image: url(rs-bg.jpg);">
+	<div class="hero-overlay bg-primary bg-opacity-80" />
+	<div class="hero-content flex-col text-center text-base-100 w-full">
+		<img class="max-w-lg" src="rs-text-white.svg" alt="Ratchadaporn & Sirakorn" />
+	</div>
+</div>
