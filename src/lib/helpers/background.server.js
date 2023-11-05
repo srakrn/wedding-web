08211e9ff2efec5648ccb93639e6ccb9ad4920f2
Sirakorn Lamyai <sirakorn.lamyai@adastragrp.com>
2023-11-05 19:45:50 +0700

    6 random images

--- src/lib/helpers/background.server.js
+++ src/lib/helpers/background.server.js
@@ -7,7 +7,7 @@ function randomIntFromInterval(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-const N_IMAGES = 5;
+const N_IMAGES = 6;
 
 const rand = randomIntFromInterval(1, N_IMAGES)
 
